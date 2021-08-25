24eb254ae67988e2eaa7f8d10c640d4a781c9796
E. Dronkert <ednl@users.noreply.github.com>
2021-08-25 13:28:25 +0200

    possibly more comparable algorithm

--- divmod.c
+++ divmod.c
@@ -1,22 +1,28 @@
 #include <stdio.h>     // printf
-#include <stdint.h>    // uint64_t
+#include <stdlib.h>    // strtoull
+#include <stdint.h>    // uint64_t, uint8_t
 #include <inttypes.h>  // PRIu64
 
-#define LOOP (5000000000ULL)
-
-int main(void)
+int main(int argc, char *argv[])
 {
-    uint64_t i = LOOP, sum = 0, a = 0, b = 1, c, q, r;
+    uint64_t a = 0, b = 1, c = 1;  // Fibonacci numbers
+    uint64_t q, r, s = 0;          // quotient, remainder, sum
+    uint64_t N = 0;                // repeat counter
+    uint8_t  f = 0;                // which Fib number to update
 
-    while (i--) {
-        c = a;
-        a = b;
-        b += c;
-        q = b / a;
-        r = b % a;
-        sum += q + r;  // avoid optimising away q and r
+    if (argc > 1) {
+        N = strtoull(argv[1], NULL, 10);
     }
 
-    printf("%"PRIu64"\n", sum);
+    while (N--) {
+        switch (f) {
+            case 0: a = b + c; q = a / c; r = a % c; f = 1; break;
+            case 1: b = c + a; q = b / a; r = b % a; f = 2; break;
+            case 2: c = a + b; q = c / b; r = c % b; f = 0; break;
+        }
+        s += q + r;  // avoid optimising away q and r
+    }
+
+    printf("%"PRIu64"\n", s);
     return 0;
 }
