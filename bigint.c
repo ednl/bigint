097c13f9728bb1cba17da712c7ff4660856b8a47
E. Dronkert <ednl@users.noreply.github.com>
2021-09-14 11:06:37 +0200

    add background qos info

--- bigint.c
+++ bigint.c
@@ -180,7 +180,8 @@ static void print(pBigInt a)
 int main(int argc, char *argv[])
 {
     #ifdef __APPLE__
-    // Set thread priority to User Interactive = use a performance core
+    // Set thread priority to QOS_CLASS_USER_INTERACTIVE = use a performance core
+    //   to use efficiency core: QOS_CLASS_BACKGROUND
     int e = pthread_set_qos_class_self_np(QOS_CLASS_USER_INTERACTIVE, 0);
     if (e) {
         printf("Pthread error: %d\n", e);
